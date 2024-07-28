25c6a1acfe886dad1f049c7f5939c4318d9b8593
Yannick <tech@yalit.be>
2024-07-28 22:25:06 +0200

    build : add doctrine fixtures

---
+++ src/Doctrine/Fixtures/UserFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
