0e86d166d693b5007c4897db5b2da1808e8271c3
Yannick <tech@yalit.be>
2024-07-30 23:20:31 +0200

    build : setup react

---
+++ assets/app.tsx
@@ -0,0 +1,16 @@
+import './styles/app.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+const App = () => {
+    return (
+        <div>
+        <h1>Hello, World!</h1>
+        </div>
+    )
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.ts')
+    .addEntry('app', './assets/app.tsx')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -60,8 +60,9 @@ Encore
     .enableTypeScriptLoader()
 
     .enablePostCssLoader()
-// uncomment if you use React
-//.enableReactPreset()
+
+    // uncomment if you use React
+    .enableReactPreset()
 
 // uncomment to get integrity="..." attributes on your script & link tags
 // requires WebpackEncoreBundle 1.4 or higher
